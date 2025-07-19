340ff74e8f8d0e43cb829a0c376a5ef0745319ac
ajadv-dev <abhijeetj26@gmail.com>
2025-07-19 23:05:44 +0200

    base path

--- src/api/consts.ts
+++ src/api/consts.ts
@@ -1,2 +1,2 @@
 export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || '';
-export const medalsApiUrl = `${BASE_PATH ?? '/medal-count-app'}/mock/medals.json`;
+export const medalsApiUrl = `${BASE_PATH}/mock/medals.json`;
