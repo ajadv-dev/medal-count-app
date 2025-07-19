220d8d33f363d2169f91f7e1898ef2ed7405a82e
ajadv-dev <abhijeetj26@gmail.com>
2025-07-19 22:50:51 +0200

    url fix

--- src/services/api.ts
+++
@@ -1,4 +0,0 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
-export const medalsApiUrl = `${basePath}/mock/medals.json`;
-console.log("Runtime medalsApiUrl:", medalsApiUrl);
--- src/services/medals/medalsService.ts
+++ src/services/medals/medalsService.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import {CountryMedals} from "@/services/medals/types";
-import {medalsApiUrl} from "@/services/api";
+import {medalsApiUrl} from "@/api/consts";
 
 export const medalsService = createApi({
     reducerPath: 'medalsService',
